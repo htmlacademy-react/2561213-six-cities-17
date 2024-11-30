5651cd8556210f73d1201cb1591b707f582c1286
Arseniy Zelenichenko <117036995+ze2ar@users.noreply.github.com>
2024-11-30 22:19:33 +0500

    feat: Add header component

--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -2,48 +2,12 @@ import React from 'react';
 
 import { HotelRating, RoomType } from '../../enums';
 import { OfferCard } from '../../components';
+import { Header } from '../../layouts';
 
 function FavoritesPage(): React.ReactElement {
   return (
     <div className='page'>
-      <header className='header'>
-        <div className='container'>
-          <div className='header__wrapper'>
-            <div className='header__left'>
-              <a className='header__logo-link' href='markup/main.html'>
-                <img
-                  className='header__logo'
-                  src='markup/img/logo.svg'
-                  alt='6 cities logo'
-                  width='81'
-                  height='41'
-                />
-              </a>
-            </div>
-            <nav className='header__nav'>
-              <ul className='header__nav-list'>
-                <li className='header__nav-item user'>
-                  <a
-                    className='header__nav-link header__nav-link--profile'
-                    href='#'
-                  >
-                    <div className='header__avatar-wrapper user__avatar-wrapper'></div>
-                    <span className='header__user-name user__name'>
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className='header__favorite-count'>3</span>
-                  </a>
-                </li>
-                <li className='header__nav-item'>
-                  <a className='header__nav-link' href='#'>
-                    <span className='header__signout'>Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main className='page__main page__main--favorites'>
         <div className='page__favorites-container container'>
@@ -61,7 +25,7 @@ function FavoritesPage(): React.ReactElement {
                 <div className='favorites__places'>
                   <OfferCard
                     key={1}
-                    imageSrc='markup/img/Apartment-small-03.jpg'
+                    imageSrc='markup/img/apartment-small-03.jpg'
                     price={180}
                     rating={HotelRating.Five}
                     text='Nice, cozy, warm big bed Apartment'
@@ -71,7 +35,7 @@ function FavoritesPage(): React.ReactElement {
                   />
                   <OfferCard
                     key={2}
-                    imageSrc='markup/img/Room-small.jpg'
+                    imageSrc='markup/img/room-small.jpg'
                     imageHeight={150}
                     imageWidth={110}
                     price={80}
